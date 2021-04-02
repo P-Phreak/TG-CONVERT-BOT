ad7351a3776271fdc0cd09351d2357525a163ae7
Predator HackerzZ <77227205+P-Phreak@users.noreply.github.com>
2021-04-02 12:52:10 +0530

    commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -12,7 +12,7 @@ from Tools.Download import download
 
 
 my_father = "https://t.me/{}".format(Config.USER_NAME[1:])
-support = "https://telegram.dog/Ns_Bot_supporters"
+support = "https://telegram.dog/TeleRoid14"
 
 @Client.on_message(Filters.command(["start"]))
 async def start(c, m):
@@ -20,7 +20,7 @@ async def start(c, m):
     await c.send_message(chat_id=m.chat.id,
                          text=Translation.START.format(m.from_user.first_name, Config.USER_NAME),
                          reply_to_message_id=m.message_id,
-                         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("⭕ Channel ⭕", url=updates_channel), InlineKeyboardButton("🛑 Support 🛑 ", url=support)]]))
+                         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("⭕ Channel ⭕", url=My Father 👨‍💻), InlineKeyboardButton("🛑 Support 🛑 ", url=support)]]))
     logger.info(f"{m.from_user.first_name} used start command")
 
 
