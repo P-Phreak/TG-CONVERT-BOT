e607ce3d1864e35754a78f276e84d3bf6e390453
Predator HackerzZ <77227205+P-Phreak@users.noreply.github.com>
2021-04-02 13:12:57 +0530

    commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -11,7 +11,7 @@ from Tools.Download import download
 
 
 
-my_father = "https://t.me/{}".format(Config.USER_NAME[1:])
+my_father = "https://t.me/{TeleRoidGroup}".
 support = "https://telegram.dog/TeleRoid14"
 
 @Client.on_message(Filters.command(["start"]))
